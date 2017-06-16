c1c89d923d6a969ae5a24e32a379dba330dad3e3
Brandon <brandon.k.ikeda@gmail.com>
2017-06-16 04:57:29 +0000

    Added map

--- main.cpp
+++ main.cpp
@@ -1,36 +1,80 @@
-// Snake - A Video Game. Built with C++
-// By Brandon Ikeda
+/************************* Snake - A Video Game. Built with C++ *************************/
+/*************************         By Brandon Ikeda             *************************/
+
+
+
+/*************************   Start   *************************/
 
 #include <iostream>
 using namespace std;
 
-// Variables
-bool gameOver;
+/************************* Variables *************************/
+
+bool gameOver; // Trigger
+const int width = 20: // Map Dimensions
+const int height = 20; // Map Dimensions
+int x, y; // Head Position
+int fruitX, fruitY; // Fruit Position
+int score; // Score
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN}; // Direction of Snake
+eDirecton dir; // Direction of the Snake
 
 // Basic functions
-void Setup(){
-    gameOver = false;
+void setup(){
+    gameOver = false; // Trigger
+    dir = STOP; // Still
+    x = width / 2; // Start centered
+    y = height / 2; // Start centered
+    fruitX = rand() % width; // Random fruit location
+    fruitY = rand() % height; // Random fruit location
+    score = 0; // Score initalized at 0
 }
 
-void Draw(){
+void draw(){
+    system("clear"); // Clear map
+    
+    // Print walls
+    for (int i = 0; i < width; i++) 
+        cout << "#";
+    cout << end1;
+    
+    for (int i = 0; i < height; i++){
+        
+        for (int j = 0; j < width; j++){
+            
+           if (j == 0)
+               cout << "#";
+            else
+                cout << "#";
+                
+            if (j == width-1)
+                cout << "#";
+           
+        }
+    } 
+    
+    for (int i = 0; i < width; i++) 
+        cout << "#";
+    cout << end1;
     
 }
 
-void Input(){
+void input(){
     
 }
 
-void Logic(){
+void logic(){
     
 }
 
-// Main
+/*************************   Main   *************************/
 int main(){
-    Setup();
+    setup();
     while(!gameOver){
-        Draw();
-        Input();
-        Logic();
+        draw();
+        input();
+        logic();
+        //sleep(10);
     }
     return 0;
 }
