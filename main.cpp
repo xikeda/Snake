df5b12e8af3bbc109d0fd7dae446306e36a7f268
Brandon <brandon.k.ikeda@gmail.com>
2017-06-16 04:16:14 +0000

    Added basic functions

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Snake - A Video Game. Built with C++
+// By Brandon Ikeda
+
+#include <iostream>
+using namespace std;
+
+// Variables
+bool gameOver;
+
+// Basic functions
+void Setup(){
+    gameOver = false;
+}
+
+void Draw(){
+    
+}
+
+void Input(){
+    
+}
+
+void Logic(){
+    
+}
+
+// Main
+int main(){
+    Setup();
+    while(!gameOver){
+        Draw();
+        Input();
+        Logic();
+    }
+    return 0;
+}
